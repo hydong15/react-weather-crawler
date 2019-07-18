6b160cd65b2a7ec885e644e7730bf247c2f0d69a
hgYim <hydong15@ajou.ac.kr>
2019-07-18 17:23:54 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
 import './App.css';
 
 class App extends Component {
@@ -11,9 +14,13 @@ class App extends Component {
     const name = "Huigyun";
 
     return (
-      <div className="App">
-        <Header name={name} foo="var"/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Header name={name} foo="bar" hello="world"/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
