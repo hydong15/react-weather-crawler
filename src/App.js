8361c186fde0c7539354bbbd9f382bdb178a789a
huigyun <48589661+hydong15@users.noreply.github.com>
2019-07-20 00:21:46 +0900

    Modify some css files (#1)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import RouterApp from './RouterApp';
 import './App.css';
